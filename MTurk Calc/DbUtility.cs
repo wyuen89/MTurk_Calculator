f3058a31b62e944d7d6d62d14ff236fe23a630f7
wyuen89 <wyuen89@gmail.com>
2018-07-19 15:40:41 -0700

    Added documentation for class variables and methods.

--- MTurk Calc/DbUtility.cs
+++ MTurk Calc/DbUtility.cs
@@ -10,10 +10,19 @@ namespace MTurk_Calc
 {
     public static class DbUtility
     {
-
+        /// <summary>
+        /// The path to the database.
+        /// </summary>
         private static String path = System.IO.Directory.GetCurrentDirectory() + "\\..\\..\\App_Data\\test.db";
+
+        /// <summary>
+        /// The connection to the SQLite database to be used.
+        /// </summary>
         private static SQLiteConnection conn = new SQLiteConnection("Data Source=" + path +";Version=3;");
 
+        /// <summary>
+        /// Creates tables if the tables do not already exist in the database. Tables are only created if this is a newly created database.
+        /// </summary>
         public static void createTables()
         {
             String createRequesters = "CREATE TABLE IF NOT EXISTS requesters (requesterID integer PRIMARY KEY, name varchar(20) NOT NULL, UNIQUE(name));";
@@ -50,6 +59,16 @@ namespace MTurk_Calc
 
         }
 
+        /// <summary>
+        /// Adds a new HIT to the database along with the requester if it isn't already exist in it's respective table.
+        /// </summary>
+        /// <param name="date">The date of the HIT</param>
+        /// <param name="requester">The requester of the HIT</param>
+        /// <param name="hitName">The HIT's name</param>
+        /// <param name="amount">The base amount paid by the HIT</param>
+        /// <param name="bonus">The bonus payment of the HIT</param>
+        /// <param name="status">Whether or not the HIT has been approved, paid, or is pending</param>
+        /// <returns>Returns true if the HIT was successfully added to the database, false otherwise.</returns>
         public static bool add(String date, String requester, String hitName, String amount, String bonus, String status)
         {
             String insertRequester = "INSERT OR IGNORE INTO requesters(name) VALUES (@requester);";
@@ -92,7 +111,6 @@ namespace MTurk_Calc
                 {
                     transaction.Rollback();
                     Console.WriteLine(e.ToString());
-                    Console.WriteLine("HIT was not recorded");
                 }
 
                 success = false;
@@ -111,6 +129,10 @@ namespace MTurk_Calc
             return success;
         }
 
+        /// <summary>
+        /// Returns an ArrayList of HitInfo objects representing all the rows in the database.
+        /// </summary>
+        /// <returns>Returns an ArrayList of HitInfo objects representing all the rows in the database.</returns>
         public static ArrayList getHITs()
         {
             String query = "SELECT hits.hitID AS id, hits.date AS \'Date\', requesters.name AS Requester, hits.hitName AS Name, hits.amount as Amount, hits.bonus AS Bonus, status.status AS Status " +
@@ -142,6 +164,11 @@ namespace MTurk_Calc
             return ret;
         }
 
+        /// <summary>
+        /// Deletes the row with the given HIT ID from the database.
+        /// </summary>
+        /// <param name="hitId">The HIT ID of the row to be deleted.</param>
+        /// <returns>Returns true if the HIT was successfully deleted, false otherwise.</returns>
         public static bool delete(String hitId)
         {
             String sql = "DELETE FROM hits WHERE hitID = @id;";
@@ -169,7 +196,6 @@ namespace MTurk_Calc
                 {
                     transaction.Rollback();
                     Console.WriteLine(e.ToString());
-                    Console.WriteLine("HIT was not deleted");
                 }
 
                 success = false;
@@ -185,14 +211,25 @@ namespace MTurk_Calc
                 conn.Close();
             }
 
-            Console.WriteLine("hit deleted");
             GalaSoft.MvvmLight.Messaging.Messenger.Default.Send<String>("deleted");
             return success;
 
         }
 
+        /// <summary>
+        /// Updates the row with the corresponding HIT ID with the param values.
+        /// </summary>
+        /// <param name="hitId">The ID of the HIT to be updated.</param>
+        /// <param name="date">The updated date.</param>
+        /// <param name="requester">The updated requester name.</param>
+        /// <param name="hitName">The updated HIT name.</param>
+        /// <param name="amount">The updated base amount of the HIT.</param>
+        /// <param name="bonus">The updated bonus amount of the HIT.</param>
+        /// <param name="status">the updated status of the HIT.</param>
+        /// <returns>Returns true if the hit was successfully updated, false otherwise.</returns>
         public static bool update(String hitId, String date, String requester, String hitName, String amount, String bonus, String status)
         {
+            //Adds updated requester name to table 'requesters' in case it it doesn't already exist.
             String insertRequester = "INSERT OR IGNORE INTO requesters(name) VALUES (@requester);";
             String sql = "UPDATE hits " +
                          "SET date = @date, requesterID = (SELECT requesterID FROM requesters WHERE name = @requester), hitName = @hitName, amount = @amount, bonus = @bonus, " +
@@ -252,11 +289,5 @@ namespace MTurk_Calc
 
             return success;
         }
-
-        public static void clearDB(SQLiteConnection conn)
-        {
-
-        }
-
     }
 }
